48555987b2e6772f9a0430f4a3f2802c008a0404
murilomhc <murilomarquess07@gmail.com>
2026-06-19 18:35:09 -0300

    feat: adiciona servico de busca de imagens da api

--- p3-pdm/App.js
+++ p3-pdm/App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity, ActivityIndicator } from 'react-native';
+import axios from 'axios';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [cats, setCats] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const fetchCats = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get('https://api.thecatapi.com/v1/images/search?limit=5');
+      setCats(response.data);
+    } catch (error) {
+      console.error("Erro ao buscar gatinhos:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchCats();
+  }, []);
